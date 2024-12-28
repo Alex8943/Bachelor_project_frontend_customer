0272a5bc6acefe7ebb0855011b6655805937b153
Alexander <65241585+Alex8943@users.noreply.github.com>
2024-12-28 16:02:03 +0100

    lets commit

--- vite.config.ts
+++ vite.config.ts
@@ -14,14 +14,6 @@ export default defineConfig({
       },
     },
   },
-  resolve: {
-    alias: {
-      util: 'rollup-plugin-node-polyfills/polyfills/util',
-      sys: 'util',
-      '@': path.resolve(__dirname, '/src'),  // Ensures '@' points to /src correctly
-    },
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],  // Ensures Vite can resolve extensions automatically
-  },
   build: {
     outDir: 'dist',  // Explicitly set output directory
     sourcemap: true,  // Helpful for debugging in production

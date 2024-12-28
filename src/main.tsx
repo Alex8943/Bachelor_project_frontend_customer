d2f3318157066cc400308b1687e550a1b2ee7d4f
Alexander <65241585+Alex8943@users.noreply.github.com>
2024-12-28 13:55:16 +0100

    lets make it work

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import * as ReactDOM from 'react-dom/client'
 import App from './App';
 
 
+
+
 const rootElement = document.getElementById('root')
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
